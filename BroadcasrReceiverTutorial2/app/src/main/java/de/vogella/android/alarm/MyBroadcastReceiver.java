153e1cc63dd416cad63864b23e3fa5ef573a22cd
winsxx <13512071@std.stei.itb.ac.id>
2015-02-09 01:24:36 +0700

    Alarm Receiver

---
+++ BroadcasrReceiverTutorial2/app/src/main/java/de/vogella/android/alarm/MyBroadcastReceiver.java
@@ -0,0 +1,21 @@
+package de.vogella.android.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    public MyBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Don't panic but your time is up!!!!.",
+                Toast.LENGTH_LONG).show();
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+    }
+}
